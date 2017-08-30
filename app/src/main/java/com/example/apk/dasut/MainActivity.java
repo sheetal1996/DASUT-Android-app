b207e893a38dcb2dcc49ce24814aec229e6f2909
Sheetal Thakkar <sheetal.thakkar@somaiya.edu>
2017-08-31 01:58:31 +0530

    List of songs, permissions request and installationtime for apps added

--- app/src/main/java/com/example/apk/dasut/MainActivity.java
+++ app/src/main/java/com/example/apk/dasut/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.apk.dasut;
 
+import android.*;
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,11 +34,19 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -43,12 +60,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
-    private TextView email;
-    private TextView apps;
-    private TextView loc2;
-    private TextView device2;
-    private TextView androidid2 ;
-    private TextView wifi2 ;
+    private TextView email, apps, loc2, device2, androidid2, wifi2, songstv;
 
     private Button pusher ;
 
@@ -62,22 +74,24 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private Double myLongitude = 0.0;
     private Double myLatitude = 0.0;
 
-    public String accountName = "abc@xyz.com";
+    public String accountName = "";
     public String MODEL ;
     public String ID ;
     public String currentDateandTime ;
     public String android_id ;
     public StringBuilder builder ;
-
-    public boolean apimmFlag = false;
-
+    private static final int PERMISSIONS_REQUEST = 133; //This could be any number, used to request permissions
+    private static final int REQUEST_CHECK_SETTINGS = 156;
+    private static final String TAG = "MainActivity";
+   // public boolean apimmFlag = false;
+   private String[] permissions = {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.GET_ACCOUNTS, android.Manifest.permission.READ_EXTERNAL_STORAGE};
     DatabaseReference firebasedata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        startService(new Intent(this, TimeService.class));
         Context context = this;
 
         email = (TextView) findViewById(R.id.email2);
@@ -102,6 +116,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             }
         });
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Note: The code below requests each permission individually
+            /* Toast.makeText(this,"SDk >  23", Toast.LENGTH_LONG).show();
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.GET_ACCOUNTS)) {
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.GET_ACCOUNTS}, PERMISSIONS_REQUEST_GET_ACCOUNTS);
+                    // MY_PERMISSIONS_REQUEST_GET_ACCOUNTS is an app-defined int constant. The callback method gets the result of the request.
+                }
+            }
+
+            if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.GET_ACCOUNTS)) {
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_GET_LOCATION);
+                    // MY_PERMISSIONS_REQUEST_GET_ACCOUNTS is an app-defined int constant. The callback method gets the result of the request.
+                }
+            }*/
+            //Request multiple permissions simulataneously
+            ActivityCompat.requestPermissions(this, permissions, PERMISSIONS_REQUEST);
+        }
+
+        /*
         if (Build.VERSION.SDK_INT < 23)
         {
             apimmFlag = false ;
@@ -110,6 +151,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         {
             apimmFlag = true ;
         }
+        */
 
         // ************DO NOT TOUCH THIS*****************
         receive(android_id);
@@ -132,14 +174,27 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         // Setting the Device and Manufacturer details
         device2.setText(MANUFACTURER+" "+MODEL+" (Android "+androidOS+")\n");
 
-        if (apimmFlag == false)
-        {
-            // Extracting user (Google) account details
-            Account account = getAccount(AccountManager.get(context));
-            accountName = account.name;
-            String fullName = accountName.substring(0, accountName.lastIndexOf("@"));
-            email.setText(accountName+"\n");
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED) {
+                // Extracting user (Google) account details
+                Account accounts[] = getAccount(AccountManager.get(getApplicationContext()));
+                for (Account account : accounts) {
+                    accountName += account.name + "\n";
+                    String fullName = accountName.substring(0, accountName.lastIndexOf("@"));
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                }
+                email.setText(accountName);
+            }
         }
+        //if (apimmFlag == false)
+        //{
+            // Extracting user (Google) account details
+            /*Account account = getAccount(AccountManager.get(context));
+            accountName += account.name;
+            String fullName = accountName.substring(0, accountName.lastIndexOf("@"));
+            email.setText(accountName+"\n");*/
+        //}
 
         // For extracting non-system applications
         List<PackageInfo> applist = getPackageManager().getInstalledPackages(0);
@@ -148,10 +203,23 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         for (PackageInfo details : applist) {
 
             if ((details.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                // if full package name is to be printed, simply append 'details'
-                builder.append(i+". "+details.applicationInfo.loadLabel(getPackageManager()).toString() + " - v" + details.versionCode + "\n");
-                i++ ;
+                //String pack = details.packageName;
+                if (Build.VERSION.SDK_INT >= 9) {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+                    String installTime = dateFormat.format( new Date( details.firstInstallTime ) );
+                    Log.d(TAG, "Installed: " + installTime);
+
+                    String updateTime = dateFormat.format( new Date( details.lastUpdateTime ) );
+                    Log.d(TAG, "Updated: " + updateTime);
+                    // if full package name is to be printed, simply append 'details'
+                    builder.append(i + ". " + details.applicationInfo.loadLabel(getPackageManager()).toString() + " - v" + details.versionCode + "\n" +installTime+"  "+updateTime+"\n");
+                    i++;
             }
+            else {
+                    builder.append(i + ". " + details.applicationInfo.loadLabel(getPackageManager()).toString() + " - v" + details.versionCode + "\n");
+                }
+        }
         }
         apps.setText(builder.toString());
 
@@ -166,8 +234,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         // Location
 
-        if (apimmFlag == false)
-        {
+        //if (apimmFlag == false)
+        //{
             loc2 = (TextView) findViewById(R.id.loc2);
             googleApiClient = new GoogleApiClient.Builder(this)
                     .addApi(LocationServices.API)
@@ -178,41 +246,125 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             locationRequest.setInterval(10 * 1000); // save battery by checking every 10 seconds
             locationRequest.setFastestInterval(15 * 1000);
             locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        }
+        //}
 
         // ---------------------------------------------------------------
 
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String permissions[], @NotNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                    Account accounts[] = getAccount(AccountManager.get(getApplicationContext()));
+                    for (Account account : accounts) {
+                        accountName += account.name + "\n";
+                        String fullName = accountName.substring(0, accountName.lastIndexOf("@"));
+                        // permission was granted, yay! Do the
+                        // contacts-related task you need to do.
+                    }
+                    email.setText(accountName);
+                } //else accountName = "Cannot access account details";
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+                    builder.setAlwaysShow(true);
+
+                    PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+                    result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                        @Override
+                        public void onResult(LocationSettingsResult result) {
+                            final Status status = result.getStatus();
+                            switch (status.getStatusCode()) {
+                                case LocationSettingsStatusCodes.SUCCESS:
+                                    Log.i(TAG, "All location settings are satisfied.");
+                                    break;
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                                    try {
+                                        // Show the dialog by calling startResolutionForResult(), and check the result
+                                        // in onActivityResult().
+                                        status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                                    } catch (IntentSender.SendIntentException e) {
+                                        Log.i(TAG, "PendingIntent unable to execute request.");
+                                    }
+                                    break;
+                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                    Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                                    break;
+                            }
+                        }
+                    });
+                }
+                /*
+                if (grantResults.length > 0 && grantResults[3] == PackageManager.PERMISSION_GRANTED) {
+                    Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    String selection = MediaStore.Audio.Media.IS_MUSIC + "!=0";
+                    Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+                    String song ="";
+                    if (cursor != null) {
+                        if (cursor.moveToFirst()) {
+                            do {
+                                String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+                                String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                                String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
+                                //SongInfo s = new SongInfo(name, artist, url);
+                                //_songs.add(s);
+                                song += title+" "+artist+" "+" "+duration+"\n";
+
+                            } while (cursor.moveToNext());
+
+                        }
+
+                        cursor.close(); }
+                    songstv = (TextView) findViewById(R.id.songs);
+                    songstv.setText(song);
+                }*/
+            }
+                //if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //  requestLocationUpdates();
+                //}
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+
 
     public void pushOnFirebase()
     {
-        if (apimmFlag == false)
-        {
+        //if (apimmFlag == false)
+        //{
             UserData userData = new UserData(accountName, android_id, MODEL, myLatitude, myLongitude, currentDateandTime, builder.toString());
             String id = firebasedata.push().getKey();
             firebasedata.child(android_id).setValue(userData);
             Toast.makeText(MainActivity.this, "Data uploaded!", Toast.LENGTH_LONG).show();
-        }
-        else
-        {
-            UserData userData = new UserData("someUser@someDomain.com", android_id, MODEL, -1.0, -1.0, currentDateandTime, builder.toString());
-            String id = firebasedata.push().getKey();
-            firebasedata.child(android_id).setValue(userData);
-            Toast.makeText(MainActivity.this, "Uploading some portion of data as API level > 23", Toast.LENGTH_LONG).show();
-        }
+        //}
+        //else
+        //{
+            //UserData userData = new UserData("someUser@someDomain.com", android_id, MODEL, -1.0, -1.0, currentDateandTime, builder.toString());
+            //String id = firebasedata.push().getKey();
+            //firebasedata.child(android_id).setValue(userData);
+            //Toast.makeText(MainActivity.this, "Uploading some portion of data as API level > 23", Toast.LENGTH_LONG).show();
+        //}
         // TODO: Add data to Firebase DB
     }
 
     // Method to get account name
-    public static Account getAccount(AccountManager accountManager) {
+    public static Account[] getAccount(AccountManager accountManager) {
         Account[] accounts = accountManager.getAccountsByType("com.google");
-        Account account;
+        /*Account account;
         if (accounts.length > 0) {
             account = accounts[0];
         } else {
             account = null;
-        }
-        return account;
+        }*/
+        return accounts;
     }
 
     public static void getInternet() {
@@ -268,7 +420,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void receive(String info_text) {
 
         DroneTest req = new DroneTest();
-        Toast.makeText(getApplicationContext(), info_text, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(), info_text, Toast.LENGTH_SHORT).show();
         req.execute(info_text);
     }
 
@@ -311,37 +463,37 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     protected void onStart() {
         super.onStart();
-        if (apimmFlag == false)
+        //if (apimmFlag == false)
             googleApiClient.connect();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (apimmFlag == false)
-        {
+        //if (apimmFlag == false)
+        //{
             if (googleApiClient.isConnected()) {
                 requestLocationUpdates();
-            }
+           // }
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (apimmFlag == false)
-        {
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-        }
+       // if (apimmFlag == false)
+        //{
+            //LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+        //}
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (apimmFlag)
-        {
+        //if (apimmFlag)
+        //{
             googleApiClient.disconnect();
-        }
+        //}
     }
 
 
@@ -414,7 +566,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         protected void onPostExecute(String result) {
             // TODO Auto-generated method stub
             // Update your UI here
-            Toast.makeText(getApplicationContext(),"Howdy!",Toast.LENGTH_SHORT).show();
+           // Toast.makeText(getApplicationContext(),"Howdy!",Toast.LENGTH_SHORT).show();
             return;
         }
     }
